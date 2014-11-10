7b2adb2665a0de198abb3cce83998b5b3bd9d1bd
Michael Ford <mtford@gmail.com>
2014-11-09 20:41:48 -0500

    Twitter follow button, about apge with video.

--- scripts/routes/About.jsx
+++ scripts/routes/About.jsx
@@ -3,7 +3,24 @@ var React = require('react');
 var About = React.createClass({
     render: function () {
         return (
-            <span>About...</span>
+            <div>
+                <h1>About</h1>
+                <div className="row">
+                    <div className="col-md-12">
+                        <p>
+                        Red-Hot-React is an opinionated boilerplate for ReactJS. It emphasises developer workflow with the aim of maximising productivity and easing delivery. See it in action below:
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="embed-responsive embed-responsive-4by3">
+                            <iframe src="//www.youtube.com/embed/dF1x3Hfg8RA" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         );
     }
 });
--- scripts/routes/Contact.jsx
+++ scripts/routes/Contact.jsx
@@ -1,11 +1,29 @@
 var React = require('react');
 
+var TwitterFollowButton = React.createClass({
+    render: function () {
+        return React.DOM.iframe({
+            src: "//platform.twitter.com/widgets/follow_button.html?screen_name=mtford90&show_count=false&show_screen_name=true",
+            scrolling: "no",
+            frameborder: "0",
+            allowTransparency: "true",
+            id: "twitter"
+        })
+    }
+});
+
+
 var Contact = React.createClass({
     render: function () {
         return (
-            <span>Contact Us!</span>
+            <div id="contact">
+                <span>Contact Us!</span>
+                <TwitterFollowButton/>
+            </div>
         );
+
     }
 });
 
 module.exports = Contact;
+
